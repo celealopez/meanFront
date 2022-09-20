123fcbdd5139995f989227d0ed053d2758dfc5e6
DESKTOP-776UT01\KDLHP07 <celeste.lopez@kdsoft.io>
2022-09-20 15:33:43 -0300

    first commit

---
+++ src/app/task/task-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainTaskComponent } from './pages/main-task/main-task.component';
+
+const routes: Routes = [{
+  path:"",
+  component:MainTaskComponent
+},
+{
+  path:"**",
+  redirectTo:"auth"
+}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TaskRoutingModule { }
